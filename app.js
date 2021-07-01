a7f54c4ea3af2022a4418dde436c2a6b90089acc
David <dada7117@gmail.com>
2021-07-01 13:27:18 +0300

    added nodemon and express ag

--- app.js
+++ app.js
@@ -1,11 +1,63 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// app.get('/', site.index);
+
+// // User
+
+// app.get('/users', user.list);
+// app.all('/user/:id/:op?', user.load);
+// app.get('/user/:id', user.view);
+// app.get('/user/:id/view', user.view);
+// app.get('/user/:id/edit', user.edit);
+// app.put('/user/:id/edit', user.update);
+
+// // Posts
+
+// app.get('/posts', post.list);
+
+
+
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
