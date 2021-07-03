8a257436eccf31d42aa6a950bc77e2c0b95dde02
David <dada7117@gmail.com>
2021-07-03 19:58:46 +0300

    Added update user

--- routes/api/members.js
+++ routes/api/members.js
@@ -32,4 +32,22 @@ router.post("/",(req,res) => {
   res.send(req.body)
 })
 
+// Update Member
+router.put("/:id", (req, res) => {
+  const found = Members.some((user) => user.id === parseInt(req.params.id));
+  if (found) {
+    const updMember = req.body;
+    console.log(updMember);
+    const member = Members.find(member => member.id === parseInt(req.params.id))
+    if(member){
+      member.name = updMember.name ? updMember.name : member.name;
+      member.email = updMember.email ? updMember.email : member.email;
+      res.json({msg:"Member updated",member})
+    }
+  }else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
+
+
 module.exports = router;
